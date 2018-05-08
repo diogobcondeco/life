6d399b846bcd4dd7c88fa1472eeb5e6cc02ce81b
Diogo Condêço <diogobcondeco@gmail.com>
2018-05-08 10:44:18 +0100

    Beginning of a journey!

---
+++ life.py
@@ -0,0 +1,48 @@
+import sys
+
+
+def drinkCoffee():
+    print ('hum, tasty coffee!')
+
+
+def eatLunch():
+    print ('let\'s lunch! awesome burger')
+
+
+def eatDinner():
+    print ('let\'s dinner! nice steak!')
+
+
+def code():
+    print ('let\'s code! print "hello world"')
+
+
+alive = True
+
+while alive == True:
+    daysToLive = 5
+    if daysToLive > 0:
+        answer = input('Is it morning, afternoon or night?')
+        if answer == 'morning':
+            print ('it\'s morning')
+            drinkCoffee()
+            drinkCoffee()
+            code()
+            print ('good morning')
+        elif answer == 'afternoon':
+            print ('it\'s afternoon')
+            drinkCoffee()
+            eatLunch()
+            code()
+            print ('good afternoon')
+        elif answer == 'night':
+            print ('it\'s night')
+            eatDinner()
+            code()
+            print ('let\'s sleep it\'s good!')
+            daysToLive -= 1
+            print ('You have', daysToLive, 'left!')
+    elif daysToLive == 0:
+        alive = False
+        print ('hello coding heaven')
+        sys.exit(0)
